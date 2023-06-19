12d6def0007e2ccc7a52524649b81d70790e3d05
Azura974 <eleahyou974@gmail.com>
2023-06-19 22:39:12 +0400

    added more !spaghetti code

--- chess.py
+++ chess.py
@@ -1,12 +1,22 @@
+#this is shit don't you judge
+#at least my code isn't spaghetti :)
 class Plateau:
     def __init__(self) -> None:
-        pass
+        board=[['♜','♞','♝','♛','♚','♝','♞','♜'],
+               ['♟','♟','♟','♟','♟','♟','♟','♟'],
+               [' ',' ',' ',' ',' ',' ',' ',' ',],
+               [' ',' ',' ',' ',' ',' ',' ',' ',],
+               [' ',' ',' ',' ',' ',' ',' ',' ',],
+               [' ',' ',' ',' ',' ',' ',' ',' ',],
+               ['♙','♙','♙','♙','♙','♙','♙','♙'],
+               ['♖','♘','♗','♕','♔','♗','♘','♖']
+               ]
 
 class Piece(Plateau):
     def __init__(self):
         self.position = []
-        self.color = 0
-        self.form=''
+        self.color = ' '
+        self.stamp=' '
 
 
     def move(self,pos):
@@ -16,8 +26,8 @@ class Pion(Piece):
     def __init__(self,color,x,y):
         self.basepos=[x,y]
         self.position=self.basepos
-        if color == 1: self.form = '' 
-        else: self.form = ''
+        if color == 'black': self.stamp = '♟' 
+        else: self.stamp = '♙'
         self.color = color
     def chckifmove(self,pos):
         possiblemoves=[[self.position[0],self.position[1]+self.color]]
